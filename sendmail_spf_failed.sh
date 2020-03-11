e7c6f1af24c3f95c33e1635d52a2d94c6a81296a
3mdf1v3 <3mdf1v3@gmail.com>
2020-03-11 15:47:33 +0100

    Update sendmail_spf_failed.sh

--- sendmail_spf_failed.sh
+++ sendmail_spf_failed.sh
@@ -6,9 +6,8 @@ DESTINATIONPATH='/root'
 DESTINATIONFILE="${DESTINATIONPATH}/spf_failed_list.txt"
 
 > ${DESTINATIONFILE}
-for i in $(grep "DNS=" /var/cpanel/users/* | awk -F '=' '{print $2}'); do
+for i in $(cat /etc/trueuserdomains | awk -F':' '{print $1}'); do
         echo "@${i}" >> ${DESTINATIONFILE}
-        grep -E "@${i}.*SPF:" /var/log/exim_mainlog | awk -F 'from ' '{print $2}' | uniq -c >> ${DESTINATIONFILE}
-        mail -s 'sendmail_spf_failed.sh' -r ${MAILSOURCE} ${MAILDESTINATION} < ${DESTINATIONFILE}  
+        grep -E "@${i}.*SPF:" /var/log/exim_mainlog | awk -F 'H=|X=|F=|:' '{print $2}' | uniq -c >> ${DESTINATIONFILE}        
 done
-
+mail -s 'sendmail_spf_failed.sh' -r ${MAILSOURCE} ${MAILDESTINATION} < ${DESTINATIONFILE}  
